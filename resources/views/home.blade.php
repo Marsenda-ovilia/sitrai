623e12b769976c4fc579320f1475a1df852c3be3
Marsenda-ovilia <marsendavilv@gmail.com>
2024-07-31 16:42:38 +0700

    tes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
 </head>
 <body>
 
+  
     <nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div class="relative flex h-16 items-center justify-between">
